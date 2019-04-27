e1914fc7a7df9d4cc73fe5e8fc2609e363c5c394
mo2561057 <moritz.mendel12@gmail.com>
2019-04-27 17:53:45 +0200

    added config file for path management

--- norpy/tests/regression_tests/run.py
+++ norpy/tests/regression_tests/run.py
@@ -12,7 +12,6 @@ from norpy.simulate_model import simulate
 from norpy.model_spec import get_random_model_specification, get_model_obj
 
 
-
 def process_arguments(parser):
     """This function parses the input arguments."""
     args = parser.parse_args()
@@ -37,7 +36,7 @@ def create_vault(num_test=100, seed=123456):
     """This function creates our regression vault."""
     np.random.seed(seed)
     seeds = np.random.randint(0, 1000, size=num_test)
-    file_dir = os.path.join(TEST_RESOURCES_DIR, "regression_vault.norpy.json")
+    file_dir = os.path.join(os.getcwd(),'ressources/regression_vault.norpy.json')
     tests = []
 
     for counter, seed in enumerate(seeds):
@@ -46,7 +45,8 @@ def create_vault(num_test=100, seed=123456):
 
 
         init_dict = get_random_model_specification()
-
+        #I sthat fine like this or should I first save to yaml and then give the input ?
+        #I dont see how that is better
         model_object = get_model_obj(init_dict)
 
         df = simulate(model_object)
@@ -59,6 +59,61 @@ def create_vault(num_test=100, seed=123456):
 
     json.dump(tests, open(file_dir, "w"))
 
+
+def check_vault():
+    """This function runs another simulation for each init file in our regression vault.
+    """
+    file_dir = os.path.join(TEST_RESOURCES_DIR, "regression_vault.soepy.json")
+
+    tests = json.load(open(file_dir, "r"))
+    for test in tests:
+
+        stat, init_dict = test
+
+        df = simulate(get_model_obj(init_dict))
+
+        stat_new = np.sum(df.sum())
+
+        np.testing.assert_array_almost_equal(stat, stat_new)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Work with regression tests for package."
+    )
+
+    parser.add_argument(
+        "--request",
+        action="store",
+        dest="request",
+        required=True,
+        choices=["check", "create"],
+        help="request",
+    )
+
+    parser.add_argument(
+        "--num", action="store", dest="num_test", type=int, help="number of init files"
+    )
+
+    parser.add_argument(
+        "--seed", action="store", dest="seed", type=int, help="seed value"
+    )
+
+    request, num_test, seed = process_arguments(parser)
+
+    if request == "check":
+        check_vault()
+
+    elif request == "create":
+
+create_vault(num_test, seed)
 
 
 
