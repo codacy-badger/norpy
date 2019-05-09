2574902a85389eb3827f92c8a7e63a5a741202b0
mo2561057 <moritz.mendel12@gmail.com>
2019-05-09 14:00:24 +0200

    set up base camp adapter

---
+++ adapter/estimation_cls.py
@@ -0,0 +1,7 @@
+"""
+What should this fiel contain ?
+We want to specify a base class that can be used for 
+estimation! 
+
+
+"""
---
+++ adapter/smm_prep.py
@@ -0,0 +1,13 @@
+"""
+What has to happen in this file.
+We have to specify functions that are crucial for the optimization routine+
+Get moments function !
+
+"""
+
+import os
+
+import numpy as np
+import pandas as pd
+
+
